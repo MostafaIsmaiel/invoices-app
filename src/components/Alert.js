182981d7f42f62adfa2ea1744b2c07ecb87291b4
Mostafaismaiel <mostafaismaiel10@gmail.com>
2021-12-03 18:42:59 +0200

    update alert animation

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -7,32 +7,34 @@ function Alert({ id }) {
   return (
     <section className="alert">
       <div className="alert__container">
-        <div className="error-sign">
-          <span className="first"></span>
-          <span className="second"></span>
-        </div>
-        <h5 className="heading">confirm deletion</h5>
-        <p className="confirm-message">
-          Are you sure you want to delete invoice {id} <br /> This action cannot
-          be undone.
-        </p>
-        <div className="actions-btn">
-          <Link
-            className="confirm btn"
-            to="/"
-            onClick={() => {
-              dispatch({
-                type: "DELETE_INVOICE",
-                payload: id,
-              });
-              setIsAlert(false);
-            }}
-          >
-            delete
-          </Link>
-          <button className="discard btn" onClick={() => setIsAlert(false)}>
-            cancel
-          </button>
+        <div className="alert-face">
+          <div className="error-sign">
+            <span className="first"></span>
+            <span className="second"></span>
+          </div>
+          <h5 className="heading">confirm deletion</h5>
+          <p className="confirm-message">
+            Are you sure you want to delete invoice {id} <br /> This action
+            cannot be undone.
+          </p>
+          <div className="actions-btn">
+            <Link
+              className="confirm btn"
+              to="/"
+              onClick={() => {
+                dispatch({
+                  type: "DELETE_INVOICE",
+                  payload: id,
+                });
+                setIsAlert(false);
+              }}
+            >
+              delete
+            </Link>
+            <button className="discard btn" onClick={() => setIsAlert(false)}>
+              cancel
+            </button>
+          </div>
         </div>
       </div>
     </section>
